f0395bd8e786edfb0f56dc229ab83dcf9fbed51d
mramshaw <mramshaw@alumni.concordia.ca>
2018-05-24 10:42:18 -0700

    Doc update

--- node.go
+++ node.go
@@ -14,13 +14,17 @@ type Node struct {
 
 // IsEntry may be called to determine if the current node is
 // terminal for an entry. Note that this node may or may not
-// also be a leaf (in the case of 'slow' and 'slowly', both
-// are entries but only 'slowly' is a leaf).
+// also be a leaf. In the case of 'slow' and 'slowly', both
+// are entries but only 'slowly' can be a leaf.
 func (n *Node) IsEntry() bool {
 	return n.entry
 }
 
-// IsLeaf may be called to determine if the current node is a leaf.
+// IsLeaf may be called to determine if the current node is a
+// leaf. Note that a leaf is a terminal node but that a node
+// may also be terminal for an entry but not a leaf. In the
+// case of 'slow' and 'slowly', 'slow' is NOT a leaf, even
+// though it is terminal for the entry 'slow'.
 func (n *Node) IsLeaf() bool {
 	return n.childCount == 0
 }
